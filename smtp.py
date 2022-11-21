3d7db094e63ae73935e100fb2c1f2821cc5da001
lqyspace <15543495640m@sina.cn>
2022-11-21 18:42:55 +0800

    发送邮件

---
+++ smtp.py
@@ -0,0 +1,62 @@
+import smtplib
+
+from email.mime.text import MIMEText
+from email.header import Header
+from email.utils import parseaddr, formataddr
+import argparse
+
+class smtpJob():
+    def __init__(self, ip):
+        self.__ip = ip
+        self.sender = "17613928573@sina.cn"
+        self.receivers = 'lqy17613928573@163.com'
+        # self.receivers = 'xiaoyun6vip@gmail.com'
+        # self.receivers = '2101738@stu.neu.edu.cn'
+        # self.receivers = 'charles_xiaoyun@qq.com'
+
+        # 第三方SMTP服务
+        self.mail_host = "smtp.sina.cn"
+        self.mail_user = "17613928573@sina.cn"
+        self.mail_pass = "075f6b98cdfcd25c"
+
+        self.send_email()
+
+    def _format_addr(self, s):
+        name, addr = parseaddr(s)
+        # print(name, addr)
+        # print(formataddr((Header(name, 'utf-8').encode(), addr)))
+        return formataddr((Header(name, 'utf-8').encode(), addr))
+
+    def send_email(self):
+        # 三个参数：第一个为文本内容，第二个plain设置文本格式，第三个 utf8 编码设置
+        message = MIMEText('实验室电脑IP：{}'.format(self.__ip), 'plain', 'utf-8')
+        message['From'] = self._format_addr('xiaoyun<{}>'.format(self.sender))
+        message['To'] = self._format_addr('lqy<{}>'.format(self.receivers))
+
+        subject = '实验室电脑IP地址：'
+        message['Subject'] = Header(subject, 'utf-8').encode()
+
+        try:
+            smtpObj = smtplib.SMTP(self.mail_host, 25)
+            smtpObj.set_debuglevel(1)
+            smtpObj.login(self.mail_user, self.mail_pass)
+            smtpObj.sendmail(self.sender, [self.receivers], message.as_string())
+            print('发送成功')
+        except smtplib.SMTPException:
+            print('Error：无法发送邮件.')
+
+# smtpObj = smtplib.SMTP(mail_host, 25)
+# smtpObj.set_debuglevel(1)
+# smtpObj.login(mail_user, mail_pass)
+# smtpObj.sendmail(sender, [receivers], message.as_string())
+# print('发送成功')
+
+if __name__ == '__main__':
+    # argparse的用法
+    parser = argparse.ArgumentParser(prog="login-NEU", description="get id_addr",
+                                     usage="%(prog)s [options]")
+    parser.add_argument('--ip', help="input IP address of %(prog)s program.", required=True)
+    args = parser.parse_args()
+
+    # 发送邮件
+    smtpJob(args.ip)
